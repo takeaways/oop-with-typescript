b8df19e1cf56015ae6bc00f41cf639f2756a41cf
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-02-09 00:15:10 +0900

    Add First Class

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Learn OOP with Typescript
 
 # OOP 4 Principles
-1. Encapsulation
-2. Abstraction
-3. Inheritance
+1. Encapsulation(캡슐화): 불필요한 내용을 외부로 노출 하지 않고 관계가 있는 속성과 함수를 잘 묶는다.
+2. Abstraction(추상화): 외부로 노출 되는 인터페이스 만으로 원하는 기능을 잘 표현할 수 있어야 한다.
+3. Inheritance:
 4. Polymorphism
--- index.ts
+++ index.ts
@@ -3,21 +3,28 @@ type CoffeeCup = {
     hasMilk:boolean;
 }
 
-const BEANS_GRAM_PER_SHOW= 7;
-let coffeeBeans = 0;
-function makeCoffee(shots: number):CoffeeCup{
-    if(coffeeBeans < shots * BEANS_GRAM_PER_SHOW){
-        throw new Error("Not Enough Coffee Beans");
+class CoffeeMaker{
+    static BEANS_GRAM_PER_SHOW= 7;
+    coffeeBeans:number = 0;
+
+    constructor(coffeeBeans:number){
+        this.coffeeBeans = coffeeBeans;
     }
 
-    coffeeBeans -= shots * BEANS_GRAM_PER_SHOW;
-
-    return {
-        shots,
-        hasMilk: false
+    makeCoffee(shots: number):CoffeeCup{
+        if(this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOW){
+            throw new Error("Not Enough Coffee Beans");
+        }
+    
+        this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOW;
+    
+        return {
+            shots,
+            hasMilk: false
+        }
     }
+    
 }
 
-coffeeBeans += 3 * BEANS_GRAM_PER_SHOW;
-const coffee = makeCoffee(2);
-console.log(coffee)
+const maker = new CoffeeMaker(32);
+console.log(maker)
